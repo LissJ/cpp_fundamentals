149f6dc59c52ba28dbdac9f249abc67a4e82a6fb
LissJ <lissj014@gmail.com>
2026-07-07 15:34:55 -0300

    guessing: adiciona leitura do primeiro palpite

--- guessing/guessing.cpp
+++ guessing/guessing.cpp
@@ -11,5 +11,15 @@ int main(){
 
     std::cout << "===== JOGO DA ADIVINHACAO =====";
 
+    std::cout << "\nTente adivinhar o numero secreto!\n";
+
+    std::cout << "Digite um numero: ";
+    std::cin >> guess;
+
+    attempts = attempts + 1;
+
+    std::cout << "\nSeu palpite foi: " << guess << std::endl;
+    std::cout << "\nTentativas: " << attempts << std::endl;
+
     return 0;
 }
