61672bb3951bc050336adba7ed103a35603f6840
LissJ <lissj014@gmail.com>
2026-07-07 11:18:39 -0300

    implementa a operação de SOMA

--- main.cpp
+++ main.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 
 int main(){
-    // eu poderia declarar o std antes, após o include do iostream, mas é uma boa prática ir utilizando conforma você faz o código
-        // exemplo de como ficaria se eu declarasse antes:
-            // using namespace std;
-    
-    // agora eu vou declarar as variáveis que vão receber o valor para realizar o cálculo
-        // eu também poderia declarar todos na mesma linha:
-            // int num1, num2;
-    int num1;       // int são números inteiros
-    int num2;
+    //apagando comentários apenas para deixar mais limpo e exercitar a memória
 
-    // o comando cout é basicamente o print do python, e o std é a biblioteca standard do c++
+    int num1;
+    int num2;
+    int resultado;
+
     std::cout << "===== CALCULADORA =====" << std::endl;
 
-    std::cout << "Digite o primeiro numero: ";
-    // cin é quando o usuário insere um número
-        // funciona como se fosse o input() do python
-            // ele pega essa inserção e armazena dentro da variável 'num1'
+    std::cout << "Insira o primeiro numero: ";
     std::cin >> num1;
 
-    std::cout << "Digite o segundo numero: ";
+    std::cout << "Insira o segundo numero: ";
     std::cin >> num2;
 
-    std::cout << std::endl;
-    
-    // com o cout, escrevemos um texto e adicionamos a variável para que ela seja exibida no visor
-        // com  isso, encerra com um endline, que nada mais é do que um \n
-    std::cout << "Primeiro: " << num1 << std::endl;
-    std::cout << "Segundo: " << num2 << std::endl;
+    resultado = num1 + num2;
 
-    // básico da estrutura
+    std::cout << "O resultado da soma: " << resultado << std::endl;
+
     return 0;
 }
