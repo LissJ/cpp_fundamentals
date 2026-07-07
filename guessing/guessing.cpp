d67007779c124f00f25f85ab3fde7c0f7b644613
LissJ <lissj014@gmail.com>
2026-07-07 15:21:27 -0300

    guessing: adiciona variáveis do jogo

--- guessing/guessing.cpp
+++ guessing/guessing.cpp
@@ -2,6 +2,13 @@
 
 int main(){
 
+    int secret_number;
+    int guess;
+    int attempts;
+
+    secret_number = 50;
+    attempts = 0;
+
     std::cout << "===== JOGO DA ADIVINHACAO =====";
 
     return 0;
