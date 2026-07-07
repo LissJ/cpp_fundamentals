aa3980192fd7d33de2673d0c5fa4a2aec2dbdcf7
LissJ <lissj014@gmail.com>
2026-07-07 10:55:49 -0300

    declaração de variáveis e armazenamento de valores

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello world! :D" << std::endl;
+    // eu poderia declarar o std antes, após o include do iostream, mas é uma boa prática ir utilizando conforma você faz o código
+        // exemplo de como ficaria se eu declarasse antes:
+            // using namespace std;
 
-    return 0
+    // o comando cout é basicamente o print do python, e o std é a biblioteca standard do c++
+    std::cout << "Primeira calculadora em C++ :D " << std::endl;
+
+    // agora eu vou declarar as variáveis que vão receber o valor para realizar o cálculo
+        // eu também poderia declarar todos na mesma linha:
+            // int num1, num2;
+    int num1;       // int são números inteiros
+
+    // cin é quando o usuário insere um número
+        // funciona como se fosse o input() do python
+            // ele pega essa inserção e armazena dentro da variável 'num1'
+    std::cin >> num1;
+    
+    // com o cout, escrevemos um texto e adicionamos a variável para que ela seja exibida no visor
+        // com  isso, encerra com um endline, que nada mais é do que um \n
+    std::cout << "Voce digitou o numero: " << num1 << std::endl;
+
+    // básico da estrutura
+    return 0;
 }
