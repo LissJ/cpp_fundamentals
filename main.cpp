e2599cf85c0536acb0c702b8a794a72a4a937313
LissJ <lissj014@gmail.com>
2026-07-07 11:31:47 -0300

    adiciona menu de escolhas de operação, porém somente com SOMA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int main(){
-    //apagando comentários apenas para deixar mais limpo e exercitar a memória
+    // apagando comentários apenas para deixar mais limpo e exercitar a memória
 
     int num1;
     int num2;
     int resultado;
+
+    // char é uma forma de armazenar apenas UM caractere, e no caso, eu irei armazenar os símbolos que serão usados nas operações matemáticas
+    int operacao;
 
     std::cout << "===== CALCULADORA =====" << std::endl;
 
@@ -15,9 +18,24 @@ int main(){
     std::cout << "Insira o segundo numero: ";
     std::cin >> num2;
 
-    resultado = num1 + num2;
+    std::cout << std::endl;
 
-    std::cout << "O resultado da soma: " << resultado << std::endl;
+    std::cout << "Escolha uma operacao: \n"
+              << "1 - Soma\n"
+              << "1 - Subtracao\n"
+              << "1 - Multiplicacao\n"
+              << "1 - Divisao\n";
+
+    std::cout << std::endl;
+
+    std::cout << "Digite aqui sua escolha: ";
+    std::cin >> operacao;
+
+    if (operacao == 1){
+        resultado = num1 + num2;
+
+        std::cout << "O resultado da soma: " << resultado << std::endl;
+    }
 
     return 0;
 }
