6e22cecc3e87be70eeb1af91db910d9b3d55103b
LissJ <lissj014@gmail.com>
2026-07-08 09:59:13 -0300

    guessing: implementação de lógica while

--- guessing/guessing.cpp
+++ guessing/guessing.cpp
@@ -5,29 +5,35 @@ int main(){
     int secret_number;
     int guess;
     int attempts;
+    bool win;
 
     secret_number = 50;
     attempts = 0;
+    win = false;
 
-    std::cout << "===== JOGO DA ADIVINHACAO =====";
+    std::cout << "===============================\n";
+    std::cout << "===== JOGO DA ADIVINHACAO =====\n";
+    std::cout << "===============================";
 
     std::cout << "\nTente adivinhar o numero secreto!\n";
 
-    std::cout << "Digite um numero: ";
-    std::cin >> guess;
+    while(win == false){
+        std::cout << "\nDigite um numero: ";
+        std::cin >> guess;
 
-    attempts = attempts + 1;
+        attempts = attempts + 1;
 
-    if (guess == secret_number){
-        std::cout << "\nParabens! Voce acertou." << std::endl;
-    }
+        if (guess == secret_number){
+            std::cout << "\nParabens! Voce acertou." << std::endl;
+        }
 
-    if (guess < secret_number){
-        std::cout << "\nO numero secreto e maior." << std::endl;
-    }
+        if (guess < secret_number){
+            std::cout << "\nO numero secreto e maior." << std::endl;
+        }
 
-    if (guess > secret_number){
-        std::cout << "\nO numero secreto é menor." << std::endl;
+        if (guess > secret_number){
+            std::cout << "\nO numero secreto e menor." << std::endl;
+        }
     }
 
     std::cout << "\nSeu palpite foi: " << guess << std::endl;
