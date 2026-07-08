ef0d355ff987f711aac5a1aa60efd4a98d639bec
LissJ <lissj014@gmail.com>
2026-07-08 13:33:48 -0300

    atm: cria estrutura inicial do projeto atm

--- atm/atm.cpp
+++ atm/atm.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <iomanip>
 
 int main(){
+    double balance;
+    int option;
+
+    // o termo 'fixed' é um manipulador de saída, que altera a forma de como os números float/double são exibidos
+        // o termo 'setprecision' também é outro manipulador de saída, que define quantas casas decimais serão exibidas
+    std::cout << std::fixed << std::setprecision(2);
+
+    std::cout << "==========================\n";
+    std::cout << "     CAIXA ELETRÔNICO\n";
+    std::cout << "==========================\n";
+
+    std::cout << "\nInforme o saldo inicial da conta: R$";
+    std::cin >> balance;
+
+    std::cout << "\nSaldo inicial: R$" << balance << std::endl;
 
     return 0;
 }
