63a68b5b055f3ca6689f57b4dae8c288108a55d1
LissJ <lissj014@gmail.com>
2026-07-08 10:51:27 -0300

    guessing: adiciona opção de jogar novamente

--- guessing/guessing.cpp
+++ guessing/guessing.cpp
@@ -8,44 +8,49 @@ int main(){
     int guess;
     int attempts;
     bool win;
+    char play_again;
 
     srand(time(nullptr));
 
-    secret_number = (rand() % 100) + 1;
+    play_again = 's';
 
-    std::cout << "Numero secreto: " << secret_number << std::endl;
+    while(play_again == 's'){
+        std::cout << "===============================\n";
+        std::cout << "===== JOGO DA ADIVINHACAO =====\n";
+        std::cout << "===============================";
 
-    attempts = 0;
-    win = false;
+        std::cout << "\nO computador escolheu um numero entre 1 e 100." << std::endl;
+        std::cout << "Tente adivinhar qual e esse numero." << std::endl;
 
-    std::cout << "===============================\n";
-    std::cout << "===== JOGO DA ADIVINHACAO =====\n";
-    std::cout << "===============================";
+        secret_number = (rand() % 100) + 1;
+        attempts = 0;
+        win = false;
 
-    std::cout << "\nO computador escolheu um numero entre 1 e 100." << std::endl;
-    std::cout << "Tente adivinhar qual e esse numero." << std::endl;
+        while(win == false){
+            std::cout << "\nDigite um numero: ";
+            std::cin >> guess;
 
-    while(win == false){
-        std::cout << "\nDigite um numero: ";
-        std::cin >> guess;
+            attempts = attempts + 1;
 
-        attempts = attempts + 1;
+            if (guess == secret_number){
+                win = true;
 
-        if (guess == secret_number){
-            win = true;
+                std::cout << "\n============================\n";
+                std::cout << "Parabens! Voce acertou." << std::endl;
+                std::cout << "Tentativas: " << attempts << std::endl;
+                std::cout << "============================\n";
 
-            std::cout << "\n============================\n";
-            std::cout << "Parabens! Voce acertou." << std::endl;
-            std::cout << "Tentativas: " << attempts << std::endl;
-            std::cout << "============================\n";
-        }
+                std::cout << "\nDeseja jogar novamente? [s/n]: ";
+                std::cin >> play_again;
+            }
 
-        if (guess < secret_number){
-            std::cout << "\nO numero secreto e maior." << std::endl;
-        }
+            if (guess < secret_number){
+                std::cout << "\nO numero secreto e maior." << std::endl;
+            }
 
-        if (guess > secret_number){
-            std::cout << "\nO numero secreto e menor." << std::endl;
+            if (guess > secret_number){
+                std::cout << "\nO numero secreto e menor." << std::endl;
+            }
         }
     }
 
